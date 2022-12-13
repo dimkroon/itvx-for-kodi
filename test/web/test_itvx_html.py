92ed578deda5732c9ca4a4605b61a2268e0cd8ae
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2022-12-13 18:24:21 +0100

    tests for categories html

--- test/web/test_itvx_html.py
+++ test/web/test_itvx_html.py
@@ -1,15 +1,19 @@
+
+# ---------------------------------------------------------------------------------------------------------------------
 #  Copyright (c) 2022 Dimitri Kroon.
 #
 #  SPDX-License-Identifier: GPL-2.0-or-later
 #  This file is part of plugin.video.itvx
-#
+# ---------------------------------------------------------------------------------------------------------------------
 
 from test.support import fixtures
 fixtures.global_setup()
 
+import time
 import unittest
 
 from resources.lib import fetch, parsex, itv_account
+from support import testutils, object_checks
 
 
 setUpModule = fixtures.setup_web_test
@@ -30,3 +34,42 @@ class TvGuide(unittest.TestCase):
         page = fetch.get_document(url)
         # testutils.save_doc(page, 'html/tv_guide.html')
         print(page)
+
+
+class Categories(unittest.TestCase):
+    all_categories = ['factual', 'drama-soaps', 'children', 'films', 'sport', 'comedy', 'news', 'entertainment']
+
+    def test_get_available_gategories(self):
+        """The page categories returns in fact already a full categorie page - the first page in the list
+        of categories. Which categorie that is, may change.
+        Maybe because of that it is much slower than requesting categories by gql.
+        """
+        t_s = time.time()
+        page = fetch.get_document('https://www.itv.com/watch/categories')
+        # testutils.save_doc(page, 'categories/categories.html')
+        t_1 = time.time()
+        data = parsex.get__next__data_from_page(page)
+        categories = data['props']['pageProps']['subnav']['items']
+        t_2 = time.time()
+        for item in categories:
+            object_checks.has_keys(item, 'id', 'name', 'label', 'url')
+            # url is the full path without domain
+            self.assertTrue(item['url'].startswith('/watch/'))
+
+        self.assertEqual(8, len(categories))        # the mobile app has an additional category AD (Audio Described)
+        self.assertListEqual([cat['label'].lower().replace(' & ', '-') for cat in categories], self.all_categories)
+        print('Categorie page fetched in {:0.3f}, parsed in {:0.3f}, total: {:0.3f}'.format(
+            t_1 - t_s, t_2 - t_1, t_2 - t_s))
+
+    def test_all_categories(self):
+        for cat in self.all_categories:
+            url = 'https://www.itv.com/watch/categories/children/' + cat
+            t_s = time.time()
+            page = fetch.get_document(url)
+            t_1 = time.time()
+            data = parsex.get__next__data_from_page(page)
+            programmes = data['props']['pageProps']['programmes']
+            t_2 = time.time()
+            self.assertIsInstance(programmes, list)
+            print("Fetched categorie {} in {:0.3f} s, parsed in {:0.3f}s, total {:0.3f}s.".format(
+                cat, t_1 - t_s, t_2 - t_1, t_2 - t_s))
