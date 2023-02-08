2e84cdbed7a8d216e5e622231d7b902dd997a965
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2023-02-08 20:09:52 +0100

    renamed lib.logging to lib.addon_log to prevent confusion with system lib.
    Signed-off-by: Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>

--- plugin.video.itvhub/addon.py
+++ plugin.video.itvhub/addon.py
@@ -1,5 +1,9 @@
-# ---------------------------------------------------------------------------------------------------------------------
-#  Copyright (c) 2022. Dimitri Kroon
+# ----------------------------------------------------------------------------------------------------------------------
+#  Copyright (c) 2022-2023 Dimitri Kroon.
+#
+#  SPDX-License-Identifier: GPL-2.0-or-later
+#  This file is part of plugin.video.itvx
+# ----------------------------------------------------------------------------------------------------------------------
 #
 #  SPDX-License-Identifier: GPL-2.0-or-later
 #  This file is part of plugin.video.itvx
@@ -7,7 +11,7 @@
 
 
 from codequick import support
-from resources.lib import logging
+from resources.lib import addon_log
 from resources.lib import main
 from resources.lib import utils
 from resources.lib import cc_patch
@@ -20,4 +24,4 @@ cc_patch.patch_label_prop()
 if __name__ == '__main__':
     utils.addon_info.initialise()
     main.run()
-    logging.shutdown_log()
+    addon_log.shutdown_log()
