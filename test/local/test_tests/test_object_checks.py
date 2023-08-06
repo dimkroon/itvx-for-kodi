36372082881ded83d259efafaaf4b7502c464f48
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2023-08-06 20:07:33 +0200

    alias imported object_checks module
    Signed-off-by: Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>

--- test/local/test_tests/test_object_checks.py
+++ test/local/test_tests/test_object_checks.py
@@ -7,40 +7,40 @@
 
 from unittest import TestCase
 
-from test.support import object_checks
+from test.support import object_checks as oc
 
 
 class TestObjectChecks(TestCase):
     def test_is_url(self):
-        self.assertTrue(object_checks.is_url('https://adskj'))
-        self.assertFalse(object_checks.is_url('http://adskj'))     # unsecure fails
-        self.assertTrue(object_checks.is_url('https://adskj/jbhnkjn.jpg', '.jpg'))
-        self.assertFalse(object_checks.is_url('https://adskj/jbhnkjn.png', '.jpg'))
-        self.assertTrue(object_checks.is_url('https://adskj/jbhnkjn.jpg?some=query&string=', '.jpg'))
-        self.assertFalse(object_checks.is_url('https://adskj/jbhnkjn.jpg?some=query&string=', '.png'))
-        self.assertTrue(object_checks.is_url('https://adskj/jbhnkjn.jpg', ('.bmp', '.jpg', '.png')))
-        self.assertFalse(object_checks.is_url('https://adskj/jbhnkjn.jpg', ('.bmp', '.gif', '.png')))
+        self.assertTrue(oc.is_url('https://adskj'))
+        self.assertFalse(oc.is_url('http://adskj'))     # unsecure fails
+        self.assertTrue(oc.is_url('https://adskj/jbhnkjn.jpg', '.jpg'))
+        self.assertFalse(oc.is_url('https://adskj/jbhnkjn.png', '.jpg'))
+        self.assertTrue(oc.is_url('https://adskj/jbhnkjn.jpg?some=query&string=', '.jpg'))
+        self.assertFalse(oc.is_url('https://adskj/jbhnkjn.jpg?some=query&string=', '.png'))
+        self.assertTrue(oc.is_url('https://adskj/jbhnkjn.jpg', ('.bmp', '.jpg', '.png')))
+        self.assertFalse(oc.is_url('https://adskj/jbhnkjn.jpg', ('.bmp', '.gif', '.png')))
 
     def test_is_iso_time(self):
-        self.assertTrue(object_checks.is_iso_utc_time("2019-05-26T23:10:32Z"))
-        self.assertFalse(object_checks.is_iso_utc_time("19-05-26T23:10:32Z"))
-        self.assertFalse(object_checks.is_iso_utc_time("2019-05-26 23:10:32Z"))
-        self.assertFalse(object_checks.is_iso_utc_time("2019-05-26T23:10:32"))
+        self.assertTrue(oc.is_iso_utc_time("2019-05-26T23:10:32Z"))
+        self.assertFalse(oc.is_iso_utc_time("19-05-26T23:10:32Z"))
+        self.assertFalse(oc.is_iso_utc_time("2019-05-26 23:10:32Z"))
+        self.assertFalse(oc.is_iso_utc_time("2019-05-26T23:10:32"))
 
-        self.assertTrue(object_checks.is_iso_utc_time("2019-05-26T23:10:32.003Z"))
-        self.assertTrue(object_checks.is_iso_utc_time("2019-05-26T23:10:32.3Z"))
-        self.assertFalse(object_checks.is_iso_utc_time("2019-05-26T23:10:32.Z"))
+        self.assertTrue(oc.is_iso_utc_time("2019-05-26T23:10:32.003Z"))
+        self.assertTrue(oc.is_iso_utc_time("2019-05-26T23:10:32.3Z"))
+        self.assertFalse(oc.is_iso_utc_time("2019-05-26T23:10:32.Z"))
 
     def test_misses_keys(self):
         d = {'a': 1, 'b': 2, 'c': 3}
-        self.assertTrue(object_checks.misses_keys(d, 'z'))
-        self.assertTrue(object_checks.misses_keys(d, 'z', 'x', 'y'))
-        self.assertRaises(AssertionError, object_checks.misses_keys, d, 'b')
-        self.assertRaises(AssertionError, object_checks.misses_keys, d, 'z', 'b')
+        self.assertTrue(oc.misses_keys(d, 'z'))
+        self.assertTrue(oc.misses_keys(d, 'z', 'x', 'y'))
+        self.assertRaises(AssertionError, oc.misses_keys, d, 'b')
+        self.assertRaises(AssertionError, oc.misses_keys, d, 'z', 'b')
 
     def test_expect_misses_keys(self):
         d = {'a': 1, 'b': 2, 'c': 3}
-        self.assertTrue(object_checks.expect_misses_keys(d, 'z'))
-        self.assertTrue(object_checks.expect_misses_keys(d, 'z', 'x', 'y'))
-        self.assertFalse(object_checks.expect_misses_keys(d, 'b'))
-        self.assertFalse(object_checks.expect_misses_keys(d, 'z', 'b'))
+        self.assertTrue(oc.expect_misses_keys(d, 'z'))
+        self.assertTrue(oc.expect_misses_keys(d, 'z', 'x', 'y'))
+        self.assertFalse(oc.expect_misses_keys(d, 'b'))
+        self.assertFalse(oc.expect_misses_keys(d, 'z', 'b'))
