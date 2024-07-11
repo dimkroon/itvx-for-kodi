077db6eba3754b99a80bdf1cb7406822b9084d6a
dimkroon <111366411+dimkroon@users.noreply.github.com>
2024-07-11 23:16:53 +0200

    skip intro
    Signed-off-by: dimkroon <111366411+dimkroon@users.noreply.github.com>

--- plugin.video.viwx/resources/lib/itv.py
+++ plugin.video.viwx/resources/lib/itv.py
@@ -17,6 +17,7 @@ from codequick.support import logger_id
 from . import utils
 from . import fetch
 from . import kodi_utils
+from .parsex import timecode2seconds
 
 
 logger = logging.getLogger(logger_id + '.itv')
@@ -115,7 +116,34 @@ def get_catchup_urls(episode_url, full_hd=False):
         subtitles = stream_data['Subtitles'][0]['Href']
     except (TypeError, KeyError, IndexError):
         subtitles = None
-    return dash_url, key_service, subtitles, playlist['VideoType'], playlist['ProductionId']
+    return dash_url, key_service, subtitles, playlist['VideoType'], playlist['ProductionId'], get_intro(stream_data)
+
+
+def get_intro(video_data):
+    """From `video_data` get the time the intro of the video ends
+
+    The data is the part of the playlist data concerning video. Depending on the type
+    of video, timecode data may or may not be present.
+
+    """
+    try:
+        timecodes = video_data.get('Timecodes')
+        if not timecodes:
+            return 0
+
+        opening_time = 0
+        recap_time = 0
+        opening_titles = timecodes.get('OpeningTitles')
+        if opening_titles:
+            opening_time = timecode2seconds(opening_titles.get('EndTime'))
+        recap = timecodes.get('Recap')
+        if recap:
+            recap_time = timecode2seconds(recap.get('EndTime'))
+
+        return max(opening_time, recap_time)
+    except:
+        logger.warning("Failed get timecodes from playlist", exc_info=True)
+        return 0
 
 
 def get_vtt_subtitles(subtitles_url):
