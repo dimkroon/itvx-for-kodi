376587cf61a275b290a49f59c8103fa41c33e800
dimkroon <111366411+dimkroon@users.noreply.github.com>
2025-02-12 19:11:49 +0100

    New tm data.
    Signed-off-by: dimkroon <111366411+dimkroon@users.noreply.github.com>

--- test/local/test_telemetry_data.py
+++ test/local/test_telemetry_data.py
@@ -1,15 +1,18 @@
 # ----------------------------------------------------------------------------------------------------------------------
-#  Copyright (c) 2024 Dimitri Kroon.
+#  Copyright (c) 2024-2025 Dimitri Kroon.
 #  This file is part of plugin.video.viwx.
 #  SPDX-License-Identifier: GPL-2.0-or-later
 #  See LICENSE.txt
 # ----------------------------------------------------------------------------------------------------------------------
+from test.support import fixtures
+fixtures.global_setup()
 
-
+import re
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 
-from test.support.object_checks import is_not_empty
+from support.object_checks import is_not_empty
+from support.testutils import doc_path
+
 
 from resources.lib import telemetry_data
 
@@ -28,3 +31,25 @@ class TestTelemetryFactory(TestCase):
         new_data = fact.get_data()
         self.assertTrue(is_not_empty(new_data, str))
         self.assertTrue(new_data in data)
+
+
+class TestaDate(TestCase):
+    def test_missing_commas(self):
+        """Ensure each line of data in the list must end with a comma, or python will
+        automatically concatenate both lines to one single string.
+
+        """
+        tm_file = doc_path('../../plugin.video.viwx/resources/lib/telemetry_data.py')
+        with open(tm_file, 'r') as f:
+            match = re.search(r'telemetry_data = \[([^]]+)]', f.read())
+        self.assertTrue(match)
+        i = 0
+        filtered_lines = 0
+        for line in match[1].splitlines():
+            i += 1
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            self.assertTrue(line.endswith(','), f'Line {i} does not end with a comma.')
+            filtered_lines += 1
+        self.assertEqual(filtered_lines, len(telemetry_data.telemetry_data))
