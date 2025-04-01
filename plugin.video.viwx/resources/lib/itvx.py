8111f1ed9308be2e269c7d904bcd7d42e378d17a
dimkroon <111366411+dimkroon@users.noreply.github.com>
2025-04-01 21:49:26 +0200

    Use regular cache for progress.
    Signed-off-by: dimkroon <111366411+dimkroon@users.noreply.github.com>

--- plugin.video.viwx/resources/lib/itvx.py
+++ plugin.video.viwx/resources/lib/itvx.py
@@ -406,15 +406,6 @@ def episodes_progress(programme_id, progress_cache=None):
         # Only save and close if the cache was opened here.
         if progress_cache is None:
             cached_pgrss.close()
-    # FIXME: This is wrong. There is no point in caching new items. The whole point of getting
-    #        progress is to sync watched state with the Kodi database. These new items will most
-    #        like have been synced, so it's no use to return them again if called shortly afterwards.
-    #        It's probably better to cache the request and perform the comparison to the progress
-    #        cache each time.
-    #        This function kinda assumes that the status of the returned episodes are successfully
-    #        synced to the Kod db. In that respect one could argue that it should return an empty
-    #        dict when it's called again before the cache expires.
-    cache.set_item(url, new_items, 300)
     return new_items
 
 
@@ -662,7 +653,7 @@ def sync_last_watched(prefer_bsl):
             log.info("No changes")
             return
 
-        # Check which programmes contain episodes that have actually changed watched status.
+        # Get the info of the programmess and their progress.
         with PersistentDict('progress.cache', 32 * 86400) as cached_progress:
             with futures.ThreadPoolExecutor(max_workers=16) as executor:
                 progress_results = [executor.submit(episodes_progress, pgm_id.replace('/', '_'), cached_progress)
@@ -673,7 +664,7 @@ def sync_last_watched(prefer_bsl):
 
         for pgm, progrss in zip((r.result() for r in programme_results),
                                 (r.result() for r in progress_results)):
-            log.info("Syncing %s episodes of programme %s", len(progrss), pgm.get('programmeId'))
+            log.info("Syncing %s episodes of programme %s", len(progrss), pgm['programme'].get('programmeId'))
             for series in pgm['seriesList']:
                 for title in series['titles']:
                     if title.get('episodeId') in progrss:
